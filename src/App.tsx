1b831c98e206036ea7628101afa443fc585403fc
KHANH LINH <linhluutk@gmail.com>
2026-02-12 11:24:41 +0700

    feat: setup react router-dom and intergrate Header into App

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './App.css'
+import Header from './components/Header/Header';
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<div>Hello World</div>} />
-        </Routes>
+      <Header />
+            <Routes>
+              <Route path="/" element={<div>Trang Overview</div>} />
+              <Route path="/task" element={<div>Trang Task Management</div>} />
+              <Route path="/calendar" element={<div>Trang Calendar</div>} />
+              <Route path="/finance" element={<div>Trang Finance</div>} />
+              <Route path="/transaction" element={<div>Trang Transactions</div>} />
+              <Route path="/dashboard" element={<div>Trang Dashboard</div>} />
+            </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   )
